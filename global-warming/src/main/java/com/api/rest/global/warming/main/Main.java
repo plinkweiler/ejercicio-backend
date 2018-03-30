d832e903e80d14895f95bcabf16bf8d7dabb79fe
plinkweiler <37916047+plinkweiler@users.noreply.github.com>
2018-03-30 03:52:06 -0300

    Add files via upload
    Se hace commit de la configuración para generar war y desplegarlo en tomcat

--- global-warming/src/main/java/com/api/rest/global/warming/main/Main.java
+++ global-warming/src/main/java/com/api/rest/global/warming/main/Main.java
@@ -4,7 +4,9 @@ import java.nio.charset.Charset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,8 +14,13 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"com.api.rest.global.warming.controller",
 		"com.api.rest.global.warming.service"})
-public class Main {
-
+public class Main extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Main.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
